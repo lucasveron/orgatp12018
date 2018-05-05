707decf6f6422c6857d84e56bac41511f1616017
Lucas Veron <veron_lucas@hotmail.com>
2018-05-04 23:23:26 -0300

    carpeta para testing

---
+++ reentrega-1/src-netbsd/src-qa/constants.h
@@ -0,0 +1,22 @@
+/*
+ * constants.h
+ *
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+
+#define MAX_LENGTH_CHARACTER	11
+#define MAX_BUFFER   		100
+
+#define TRUE 		 	1
+#define FALSE 		 	0
+
+/* State */
+#define	OKEY			0
+#define ERROR_FILE		1
+#define ERROR_MEMORY		2
+#define ERROR_WRITE		3
+
+#endif /* CONSTANTS_H_ */
