03b1a1036b623f16b4dc9c91bebf5acb25df7a8e
Lucas Veron <veron_lucas@hotmail.com>
2018-05-05 17:04:11 -0300

    agrego test diff imagenes

--- reentrega-1/src-netbsd/src-qa/automatic-tests.sh
+++ reentrega-1/src-netbsd/src-qa/automatic-tests.sh
@@ -16,7 +16,8 @@ else
 fi
 
 echo
-echo "###----------###    COMIENZA la generacion de imagenes automaticamente.    ###----------###"
+echo "################ Comienza la generacion de imagenes automaticamente ##########"
+echo
 
 ./tp1 -r 50x50 --center 0.5+0.5i -w 25 --height 10 --output ./outputs-automatic-tests/prueba1.pgm -m mips32
 echo "Se genero el archivo prueba1.pgm, para lo cual se especifico center, w, height, s y output."
@@ -37,7 +38,7 @@ echo "Se genero el archivo prueba5.pgm, para lo cual se especifico resolution, c
 echo "Se genero el archivo prueba6.pgm, para lo cual se especifico resolution, c, width y o."
 
 ./tp1 --resolution 54x67 -c -0.956+0.15236i --width 3 --height 10 -o ./outputs-automatic-tests/prueba7.pgm -m mips32
-echo "Se genero el archivo prueba6.pgm, para lo cual se especifico resolution, c, width, height y o."
+echo "Se genero el archivo prueba7.pgm, para lo cual se especifico resolution, c, width, height y o."
 
 ./tp1 --resolution 35x52 -c -0.5201-0.15236i --width 3 --height 5 -m mips32 -o - > ./outputs-automatic-tests/outputStdout.pgm
 echo "Se ejecuto con salida estandar, que se redirecciono al archivo outputStdout.pgm, para lo cual se especifico resolution, c, width, height y o (stdout, como - )."
@@ -51,23 +52,48 @@ echo "Se genero el archivo dos.pgm, para lo cual se especifico c, w, H y o."
 ./tp1 -r 20x20 -m mips32 -o ./outputs-automatic-tests/unoMips32.pgm -m mips32
 echo "Se genero el archivo unoMips32.pgm, para lo cual se especifico m y o"
 echo
-echo "##############################################################################"
-echo "################ COMPARACION DE METODOS DE EJECUCION #########################"
-echo "##############################################################################"
+echo "################ Comparacion de metodos de ejecucion #########################"
 echo
+
+echo "Generación de imagen estandar 10x10 ..."
 ./tp1 -r 10x10 -m mips32 -o ./outputs-automatic-tests/comp_mips32_1.pgm
 ./tp1 -r 10x10 -m generic -o ./outputs-automatic-tests/comp_generic_1.pgm
 
-varDiff = $(diff ./outputs-automatic-tests/comp_mips32_1.pgm ./outputs-automatic-tests/comp_generic_1.pgm 2>&1)
+varDiff=$(diff ./outputs-automatic-tests/comp_mips32_1.pgm ./outputs-automatic-tests/comp_generic_1.pgm 2>&1)
 if [ "$varDiff" = "" ];
- then echo "Ejecucion Diff .......... [OK]";
+ then echo "Ejecucion diff 1 ................................... [OK]";
 else
- echo "Ejecucion Diff .......... [ERROR] - Resultado obtenido: ";
+ echo "Ejecucion test diff 1 .............................. [ERROR] - Resultado obtenido: ";
  echo $varDiff
 fi
 
-echo "###----------###    FIN de la generacion de imagenes automaticamente.    ###----------###"
 echo
-echo "###------------------------------------------------------------------------###"
-echo "###------------------------------------------------------------------------###"
-echo "###------------------------------------------------------------------------###"
+echo "Generación de imagen estandar 100x100 ..."
+./tp1 -r 50x50 -m mips32 -o ./outputs-automatic-tests/comp_mips32_2.pgm
+./tp1 -r 50x50 -m generic -o ./outputs-automatic-tests/comp_generic_2.pgm
+
+varDiff=$(diff ./outputs-automatic-tests/comp_mips32_2.pgm ./outputs-automatic-tests/comp_generic_2.pgm 2>&1)
+if [ "$varDiff" = "" ];
+ then echo "Ejecucion test diff 2 .............................. [OK]";
+else
+ echo "Ejecucion Diff .............................. [ERROR] - Resultado obtenido: ";
+ echo $varDiff
+fi
+
+echo
+echo "Generación de imagen estandar 255x255 ..."
+./tp1 -r 255x255 -m mips32 -o ./outputs-automatic-tests/comp_mips32_3.pgm
+./tp1 -r 255x255 -m generic -o ./outputs-automatic-tests/comp_generic_3.pgm
+
+varDiff=$(diff ./outputs-automatic-tests/comp_mips32_3.pgm ./outputs-automatic-tests/comp_generic_3.pgm 2>&1)
+if [ "$varDiff" = "" ];
+ then echo "Ejecucion test diff 3 .............................. [OK]";
+else
+ echo "Ejecucion Diff .............................. [ERROR] - Resultado obtenido: ";
+ echo $varDiff
+fi
+
+echo
+echo "##############################################################################"
+echo "######################## Fin tests automaticos  ##############################"
+echo "##############################################################################"
